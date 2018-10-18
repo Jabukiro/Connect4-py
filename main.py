2d69408d4b4a7ad2fc5a3140da7c88691736bec3
Barihuta Hervé-Daniel <d.barihuta@gmail.com>
2018-10-18 19:11:55 +0800

    Make backpropagation() recursive

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import sys
 sys.path.insert(0, 'Lib/site-packages/')
 import numpy as np
 import json
-#TODO: Initialise input in neuron activation
+#TODO: Update main()
+#TODO: Consider assigning automatic values to backpropagation param so that it can be called args free
+#TODO: Consider calling update() inside backpropagation(). Check error free first.
 
 class NeuralNetwork:
     features = 42
-    WIDTH = 7
+    OUT = 1
     row = 6
     neurons = 60
     Epoch = 0
@@ -23,18 +25,26 @@ class NeuralNetwork:
             self.Games_Num = option['GameSets']
             self.X = np.array(option['input']) #Input
             self.Yd = np.array(option['output'])#Desired output
-            self.Y_InOutErr = np.zeros((3,7))#Output neurons
-            self.h_lay= 1 #Number of hidden layers
+            self.Y_InOutErr = np.zeros((3,OUT))#Output neurons
+            self.totalLayers= option['Layers'] #Number layers
             self.h1_InOutErr = np.zeros((3,neurons))#hidden layer neurons
             self.err_out = np.zeros(option['output'])#error between output and desired output
             
             self.W_ih =  np.random.normal(0.0, 1.0/np.sqrt(features), (neurons, features)) #Weights between input and hidden layer.
             self.T_h = np.random.normal(0.0, 1.0/np.sqrt(features), neurons) #Hidden layer neurons' tresholds 
-            self.W_ho = np.random.normal(0.0, 1.0/np.sqrt(features), (neurons, WIDTH))
-            self.T_o = np.random.normal(0.0, 1.0/np.sqrt(features), WIDTH)
-            L_LIST = (self.X, self.h1_InOutErr, self.Y_InOutErr)
-            W_LIST = (self.W_ih, self.W_ho)
-            T_LIST = (self.T_h, self.T_o)
+            self.W_ho = np.random.normal(0.0, 1.0/np.sqrt(features), (neurons, OUT))
+            self.T_o = np.random.normal(0.0, 1.0/np.sqrt(features), OUT)
+            self.layList = [self.X, self.h1_InOutErr, self.Y_InOutErr]
+            self.weiList = [self.W_ih, self.W_ho]
+            self.treList = [self.T_h, self.T_o]
+
+    def update(self, cmd, container):
+        if cmd = "inputs":
+            for i in range(self.totalLayers):
+                self.layList[i] = container[i]
+        if cmd = "weights":
+            for i in range(self.totalLayers-1):
+                self.weiList[i] = container[i]
 
     def NeuronsActivation(self, container, lidx, widx, tidx):
         #ref: TB p177 eq:6.96
@@ -43,7 +53,7 @@ class NeuralNetwork:
            self.NeuronsActivation(container, lidx+1, widx, tidx)
            
         elif (lidx <=2):
-            inp, weights, tresholds = L_LIST[lidx], W_LIST[widx], T_LIST[tidx]
+            inp, weights, tresholds = self.layList[lidx], self.weiList[widx], self.treList[tidx]
 
             #Calculation of the input to each neuron first
             tresholds = np.reshape(tresholds, (np.shape(tresholds)[0], 1)) #Turning from a 1D array into a matrix
@@ -58,25 +68,38 @@ class NeuralNetwork:
             
     
     def outErr(self, cmd=None):
-        self.Y_InOutErr[2] = np.subtract(self.Y[1], self.Yd)
+        self.Y_InOutErr[2] = np.subtract(self.Y_InOutErr[1], self.Yd)
 
-    def backPropagation(self, inpNeur, weights, outNeur):
-        """ Updates weights between hiddenb and output layer
-        @inpNeur: input
+    def backPropagation(self, container, err, lidx, widx, tidx):
+        """ Recursive Function. Returns list containing new weights for next iteration, for all weights.
+            Only needed to be called once per iteration.
+            Should call self.update() next to actually update the
+            #ref p177
         """
-        #TODO: compute the error for the next error gradient calculation, so as to implement the self calling method easily.
         #ref p177
-        errGradient = np.zeros(np.shape(weights)[1])
-        errGradient = np.subtract(1, np.multiply(outNeur[1], outNeur[1])*outNeur[2])
-        inpNeurT = np.reshape(inpNeur[1], (7,1))
-        weightCorr = np.multiply(learnrate, np.multiply(inpNeurT, errGradient))
-        weights = np.add(weights, weightCorr)
+        
+        #Output Layer Error Gradient calculation
+        if lidx = len(self.totalLayers)-1:
+            errGradient = np.zeros(np.shape(self.layList[lidx][2])[1])
+            errGradient = np.subtract(1, np.multiply(self.layList[lidx][1], self.layList[lidx][1]))*err
+        #Middle layer Error Gradient Calculation
+        elif lidx >0:
+            errGradient = 1-np.multiply(np.substract(1, np.multipy(outNeur[1], outNeur[1])), err))
 
-    def backPropagationMid(self, inpNeur = self.X, weights =self.W_ih, outNeur = self.n_InOutErr, weights2 = self.W_ho, outNeur2 = self.Y_InOutErr):
-        outNeur[2] = 1-np.multiply(np.substract(1, np.multipy(outNeur[1], outNeur[1])), np.sum(np.multiply(outNeur2[2], weights2), axis=0))
-        inpNeurT = np.reshape(inpNeur[1], (60,1))
-        weightCorr = np.multiply(learnrate, np.multiply(inpNeurT, outNeur[2]))
-        weights = np.add(weights, weightCorr)
+        #Rest is similar for all layers
+        inpNeurT = np.reshape(self.layList[lidx-1][1], (OUT,1))
+        weightCorr = np.multiply(learnrate, np.multiply(inpNeurT, errGradient))
+        weights = np.add(self.weiList[widx], weightCorr)
+
+        container.append(weights)
+        self.layList[lidx][2] = errGradient
+
+        if (widx == 0): #Means no more weights needed to be calculated
+            return container
+
+        err = np.sum(np.multiply(self.layList[lidx][2], self.weiList[widx]), axis=0
+        backPropagation(container, err, lidx-1, widx-1, tidx-1)
+
 
 def main():
     with open('option.txt', 'r') as file:
