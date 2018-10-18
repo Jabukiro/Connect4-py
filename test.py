e706e8edeba428c39fd00d7e6d233beb0508106a
Barihuta Hervé-Daniel <d.barihuta@gmail.com>
2018-10-18 17:37:25 +0800

    Update the secondary tools

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import sys
-sys.path.insert(0, 'Lib/site-packages/')
-import tensorflow as tf
 import numpy as np
 
-def in_sum(self):
-    buffer = 0
-    for w in range(neurons):
-        self.neuron[neurons] = tf.accumulate(tf.multiply(self.X, self.W1[neurons], name="nOUT"))
+a = np.random.normal(0.0, 1.0/np.sqrt(42), (60, 7))
+b = np.random.normal(0.0, 1.0/np.sqrt(42), 7)
+
+def change(sub, testy):
+    return sub
+
+c = change(a, b)
