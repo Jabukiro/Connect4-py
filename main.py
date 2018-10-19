1e385cef742a670ab2da78521a1358877cb3988e
Barihuta Hervé-Daniel <d.barihuta@gmail.com>
2018-10-19 17:23:00 +0800

    Render the ANN stable and error free

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 #Implementation of the classic Connect4 using an ANN. Reference used is "Artificial Inteligence: A Guide to Intelligent Systems"
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
-import sys
-sys.path.insert(0, 'Lib/site-packages/')
 import numpy as np
 import json
+#TODO: Can't convert ndray to JSON. Fix it
 #TODO: Add error plotting.
 #TODO: Implement momentum.
 #TODO: Implement play option.
@@ -18,123 +14,146 @@ class NeuralNetwork:
     row = 6
     neurons = 60
     Epoch = 0
-    MaxEpoch = 500
+    MaxEpoch = 50
     iteration = 0
     learnRate = .1
-    __init__(self, option, data):
-        if (option['mode'] = 'train'):
-            self.Games_Num = option['GameSets']
-            self.X = np.array(option['input']) #Input
-            self.Yd = np.array(option['output'])#Desired output
-            self.Y_InOutErr = np.zeros((3,OUT))#Output neurons
+    
+    def __init__(self, option, data):
+        if (option['mode'] == 'train'):
+            self.Games_Num = len(data)
+            self.X = np.zeros((2,option['input'])) #Input
+            self.Yd = np.zeros((2, option['output']))#Desired output
+            self.Y_InOutErr = np.zeros((3, NeuralNetwork.OUT))#Output neurons
             self.totalLayers= option['Layers'] #Number layers
-            self.h1_InOutErr = np.zeros((3,neurons))#hidden layer neurons
+            self.h1_InOutErr = np.zeros((3, NeuralNetwork.neurons))#hidden layer neurons
             self.err_out = np.zeros(option['output'])#error between output and desired output
             
-            self.W_ih =  np.random.normal(0.0, 1.0/np.sqrt(features), (neurons, features)) #Weights between input and hidden layer.
-            self.T_h = np.random.normal(0.0, 1.0/np.sqrt(features), neurons) #Hidden layer neurons' tresholds 
-            self.W_ho = np.random.normal(0.0, 1.0/np.sqrt(features), (neurons, OUT))
-            self.T_o = np.random.normal(0.0, 1.0/np.sqrt(features), OUT)
+            self.W_ih =  np.random.normal(0.0, 1.0/np.sqrt(NeuralNetwork.features), (NeuralNetwork.neurons, NeuralNetwork.features)) #Weights between input and hidden layer.
+            self.T_h = np.random.normal(0.0, 1.0/np.sqrt(NeuralNetwork.features), NeuralNetwork.neurons) #Hidden layer neurons' tresholds 
+            self.W_ho = np.random.normal(0.0, 1.0/np.sqrt(NeuralNetwork.features), (NeuralNetwork.OUT, NeuralNetwork.neurons))
+            self.T_o = np.random.normal(0.0, 1.0/np.sqrt(NeuralNetwork.features), NeuralNetwork.OUT)
             self.layList = [self.X, self.h1_InOutErr, self.Y_InOutErr]
             self.weiList = [self.W_ih, self.W_ho]
             self.treList = [self.T_h, self.T_o]
 
     def update(self, cmd, container):
-        if cmd = "inputs":
+        if (cmd == "outputs"):
             for i in range(self.totalLayers):
-                self.layList[i] = container[i]
-        if cmd = "weights":
+                self.layList[i][1] = container[i]
+        if (cmd == "weights"):
             for i in range(self.totalLayers-1):
-                self.weiList[i] = container[i]
+                j = self.totalLayers-2
+                self.weiList[i] = container[j-i]#Weights in container are in reverse order
         return
 
-    def NeuronsActivation(self, container[], lidx=0, widx=0, tidx=0):
+    def NeuronsActivation(self, data = [], container=[], lidx=0, widx=0, tidx=0):
         #ref: TB p177 eq:6.96
-        if lidx = 0:
-           container.append()
-           self.NeuronsActivation(container, lidx+1, widx, tidx)
+        if (lidx == 0):
+            container = []
+            container.append(data[:len(data)-1])
+            self.layList[lidx][0] = container[0]
+            self.layList[lidx][1] = container[0]
+            self.Yd[0] = data[len(data)-1]
+            self.NeuronsActivation(container =container, lidx =lidx+1, widx =widx, tidx =tidx)
            
-        elif (lidx <=2):
+        elif (lidx <=self.totalLayers-1):
             inp, weights, tresholds = self.layList[lidx], self.weiList[widx], self.treList[tidx]
 
             #Calculation of the input to each neuron first
             tresholds = np.reshape(tresholds, (np.shape(tresholds)[0], 1)) #Turning from a 1D array into a matrix
-            neurons[0] = np.sum(np.subtract(np.multiply(weights, inp), tresholds), axis=1)
+            inp[0] = np.sum(np.subtract(np.multiply(weights, container[lidx-1]), tresholds), axis=1)
+            self.layList[lidx][0] = inp[0]
 
             #Activation function is tanh which is a rescaling of the Sigmoid over -1 to 1.
-            neurons[1] = np.tanh(neurons[0])
+            inp[1] = np.tanh(inp[0])
 
-            container.append(neurons[1])
-            self.NeuronsActivation(container, lidx+1, widx+1, tidx+1)
+            container.append(inp[1])
+            self.NeuronsActivation(container =container, lidx =lidx+1, widx =widx+1, tidx =tidx+1)
         return container
             
     
     def outErr(self, cmd=None):
-        self.Y_InOutErr[2] = np.subtract(self.Y_InOutErr[1], self.Yd)
-        return
+        self.Yd[1] = np.subtract(self.layList[self.totalLayers-1][1], self.Yd[0])
+        return self.Yd[1]
 
-    def backPropagation(self, err, container=[], lidx=self.totalLayers-1, widx=self.totalLayers-2, tidx=self.totalLayers):
+    def backPropagation(self, err, container=[], lidx= None, widx=None, tidx=None):
         """ Recursive Function. Returns list containing new weights for next iteration, for all weights.
             Only needed to be called once per iteration.
             Should call self.update() next to actually update the
             #ref p177
         """
         #ref p177
-        
         #Output Layer Error Gradient calculation
-        if lidx = len(self.totalLayers)-1:
-            errGradient = np.zeros(np.shape(self.layList[lidx][2])[1])
+        if (lidx is None):
+            lidx, widx, tidx= self.totalLayers-1, self.totalLayers-2, self.totalLayers
+            errGradient = np.zeros(np.shape(self.layList[lidx][2])[0])
             errGradient = np.subtract(1, np.multiply(self.layList[lidx][1], self.layList[lidx][1]))*err
         #Middle layer Error Gradient Calculation
-        elif lidx >0:
-            errGradient = 1-np.multiply(np.substract(1, np.multipy(outNeur[1], outNeur[1])), err))
+        elif (lidx >0):
+            errGradient = 1-np.multiply(np.subtract(1, np.multiply(self.layList[lidx][1], self.layList[lidx][1])), err)
 
         #Rest is similar for all layers
-        inpNeurT = np.reshape(self.layList[lidx-1][1], (OUT,1))
-        weightCorr = np.multiply(learnrate, np.multiply(inpNeurT, errGradient))
+        inpNeurT = self.layList[lidx-1][1]
+        errGradient = np.reshape(errGradient, (np.shape(errGradient)[0], 1))
+        weightCorr = np.multiply(NeuralNetwork.learnRate, np.multiply(inpNeurT, errGradient))
         weights = np.add(self.weiList[widx], weightCorr)
 
         container.append(weights)
-        self.layList[lidx][2] = errGradient
+        self.layList[lidx][2] = np.reshape(errGradient, np.shape(errGradient)[0])
 
-        if (widx == 0): #Means no more weights needed to be calculated
-            return container
+        if (widx > 0): #Means no more weights needed to be calculated
+            err = np.sum(np.multiply(self.layList[lidx][2], self.weiList[widx]), axis=0)
+            self.backPropagation(err, container, lidx-1, widx-1, tidx-1)
 
-        err = np.sum(np.multiply(self.layList[lidx][2], self.weiList[widx]), axis=0
-        backPropagation(container, err, lidx-1, widx-1, tidx-1)
-
-    def saveState(self):
-        weights = []
+        return container
+    def saveState(self, options):
+        weights =[]
         tresholds = []
-        for i in range(self.totalLayers-1):
-            weights = self.weiList[i]
-            tresholds = self.treList[i]
-
+        for i in range(self.totalLayers-2):
+            weights.append(np.ndarray.tolist(self.weiList[i]))
+            tresholds.append(np.ndarray.tolist(self.treList[i]))
         options['weights'] = weights
         options['tresholds'] = tresholds
-        with open('option.json', 'w') as file:
-            json.dump(option, file)
+        with open('options.json', 'w') as file:
+            json.dump(options, file, indent=4)
         return
 
+def makeInt(c):
+    for i in range(len(c)):
+        line = c[i].split(",")
+        c[i] = []
+        for j in range(len(line)):
+            c[i].append(int(line[j]))
+    return c
 
 def main():
-    with open('option.json', 'r') as file:
-        option = json.load(file)
+    
+    with open('options.json', 'r') as file:
+        options = json.load(file)
+    print("loaded options")
     with open('data.txt', 'r') as file:
         content = file.read()
-        data = c.split().split(',')
-    ann = NeuralNetwork(option, data)#Step1&2: Initialisation
-    while (NeuralNetwork.Epoch <= NeuralNetwork.MaxEpoch):
-        while(ann.iteration <= ann.Games_Num):
+        data = makeInt(content.split())
+        print("loaded data")
+
+    ann = NeuralNetwork(options, data)#Step1&2: Initialisation
+    while (NeuralNetwork.Epoch < NeuralNetwork.MaxEpoch):
+        while(NeuralNetwork.iteration < ann.Games_Num):
             #Activation of weights and thresholds
-            ann.NeuronsActivation()
+            outputs = ann.NeuronsActivation(data[NeuralNetwork.iteration])#Returns outputs of each neuron
+            ann.update("outputs", outputs)
+
             #Weight training
             error = ann.outErr()
-            ann.backPropagation(error)
+            w = ann.backPropagation(error)
+            ann.update("weights", w)
+
             NeuralNetwork.iteration +=1
         NeuralNetwork.Epoch +=1
-    ann.saveState()
-    
+        print(NeuralNetwork.Epoch)
+        print(ann.Yd[1])#Problem with this
+    ann.saveState(options)
+
 if __name__ == '__main__':
     #Main will not run if NeuralNetwork is imported as a module
     #Can they be used to play by loading a saved state
