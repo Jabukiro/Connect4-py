c26c30101331ba926ee14199bec24411dec4a70c
Barihuta Hervé-Daniel <d.barihuta@gmail.com>
2018-10-31 22:18:51 +0800

    Cleaning out index

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-a = np.random.normal(0.0, 1.0/np.sqrt(42), (60, 7))
-b = np.random.normal(0.0, 1.0/np.sqrt(42), 7)
-
-def change(sub, testy):
-    return sub
-
-c = change(a, b)
